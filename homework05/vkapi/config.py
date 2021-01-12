de67b707a73307cc1196bce4dda5ecfd4dc87d8d
DamirZv <dzd2002@yandex.ru>
2021-01-12 22:19:23 +0300

    1

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "1809bbe24c8f600bb41ec0cabe2e943f677028b9c202aa8421bf5dc63fa251089162789c80c9cd3220e0f",
     "version": "5.126",
 }
