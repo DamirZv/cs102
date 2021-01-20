22dc7a9e6d51eb6bd4e31817d4d8b109bd019f1b
DamirZv <dzd2002@yandex.ru>
2021-01-21 01:51:29 +0300

    исправлены ошибки

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -17,7 +17,7 @@ def ego_network(
     :param user_id: Идентификатор пользователя, для которого строится граф друзей.
     :param friends: Идентификаторы друзей, между которыми устанавливаются связи.
     """
-    script = []
+    graph = []
     if friends is None:
         fields: tp.List[tp.Dict[str, tp.Any]] = get_friends(user_id, fields=["nickname", "is_closed, deactivate"]).items  # type: ignore
         friends = [
@@ -29,34 +29,34 @@ def ego_network(
     for mutual in mutuals:
         mut = tp.cast(MutualFriends, mutual)
         for common in mut["common_friends"]:
-            script.append((mut["id"], common))
-    return script
+            graph.append((mut["id"], common))
+    return graph
 
 
 def plot_ego_network(net: tp.List[tp.Tuple[int, int]]) -> None:
-    script = nx.Graph()
-    script.add_edges_from(net)
-    layout = nx.spring_layout(script)
-    nx.draw(script, layout, node_size=10, node_color="black", alpha=0.5)
+    graph = nx.Graph()
+    graph.add_edges_from(net)
+    layout = nx.spring_layout(graph)
+    nx.draw(graph, layout, node_size=10, node_color="black", alpha=0.5)
     plt.title("Ego Network", size=15)
     plt.show()
 
 
 def plot_communities(net: tp.List[tp.Tuple[int, int]]) -> None:
-    script = nx.Graph()
-    script.add_edges_from(net)
-    layout = nx.spring_layout(script)
-    partition = community_louvain.best_partition(script)
-    nx.draw(script, layout, node_size=25, node_color=list(partition.values()), alpha=0.8)
+    graph = nx.Graph()
+    graph.add_edges_from(net)
+    layout = nx.spring_layout(graph)
+    partition = community_louvain.best_partition(graph)
+    nx.draw(graph, layout, node_size=25, node_color=list(partition.values()), alpha=0.8)
     plt.title("Ego Network", size=15)
     plt.show()
 
 
 def get_communities(net: tp.List[tp.Tuple[int, int]]) -> tp.Dict[int, tp.List[int]]:
     communities = defaultdict(list)
-    script = nx.Graph()
-    script.add_edges_from(net)
-    partition = community_louvain.best_partition(script)
+    graph = nx.Graph()
+    graph.add_edges_from(net)
+    partition = community_louvain.best_partition(graph)
     for uid, cluster in partition.items():
         communities[cluster].append(uid)
     return communities
